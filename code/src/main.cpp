9c85f3614aa5a1f05e4cd03dcec9bd3170667bb9
slinkyLynx <slinkylynx@proton.me>
2024-08-31 00:17:05 +0100

    commented opengl-specific code and unused parameters out

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -7,10 +7,10 @@
 #include <glm/glm.hpp>
 
 void error_callback(int error, const char* description) {
-	fprintf(stderr, "Error: %s\n", description);
+	std::cerr << "Error: " << error << '\n' << description << std::endl;
 }
 
-void window_close_callback(GLFWwindow* window) {
+void window_close_callback(GLFWwindow* /*window*/) {
 	std::cout << "Closing window!" << std::endl;
 }
 
@@ -27,7 +27,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 }
 
-int main(int argc, char* argv[]) {
+int main(int /*argc*/, char* /*argv*/[]) {
 	glfwSetErrorCallback(error_callback);
 	
 	if (!glfwInit()) {
@@ -36,7 +36,8 @@ int main(int argc, char* argv[]) {
 	
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+//	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	
 	GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW test window", nullptr, nullptr);
 	
@@ -48,8 +49,8 @@ int main(int argc, char* argv[]) {
 	glfwSetWindowCloseCallback(window, window_close_callback);
 	glfwSetKeyCallback(window, key_callback);
 	
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
+//	glfwMakeContextCurrent(window);
+//	glfwSwapInterval(1);
 	
 	while (!glfwWindowShouldClose(window)) {
 //		int width, height;
@@ -59,7 +60,7 @@ int main(int argc, char* argv[]) {
 		double time = glfwGetTime();
 		std::cout << time << std::endl;
 		
-		glfwSwapBuffers(window);
+//		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	
